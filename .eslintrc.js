61207f770cdff287e76327eec96988c8a06dd5f5
Erik Marks <rekmarks@protonmail.com>
2020-05-11 16:51:33 -0700

    remove eslintrc cruft

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es6: true,
-  },
-  extends: [
-    '@metamask/eslint-config',
-    '@metamask/eslint-config/config/nodejs',
-  ],
-  plugins: [
-    'json',
-  ],
-  globals: {
-    document: false,
-    navigator: false,
-    web3: true,
-    window: false,
-    $: false,
-    QUnit: false,
   },
   parserOptions: {
     ecmaVersion: 2018,
   },
+  plugins: [
+    'json',
+  ],
+  extends: [
+    '@metamask/eslint-config',
+    '@metamask/eslint-config/config/nodejs',
+  ],
   ignorePatterns: [
     '!.eslintrc.js',
     'node_modules/',
