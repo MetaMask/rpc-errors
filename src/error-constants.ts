0cf1f9af91c812453c449ed200396da41d3579e2
Erik Marks <rekmarks@protonmail.com>
2020-11-01 08:03:55 -0800

    Add interface for errorCodes

--- src/error-constants.ts
+++ src/error-constants.ts
@@ -1,4 +1,27 @@
-export const errorCodes = {
+interface ErrorCodes {
+  readonly rpc: {
+    readonly invalidInput: -32000;
+    readonly resourceNotFound: -32001;
+    readonly resourceUnavailable: -32002;
+    readonly transactionRejected: -32003;
+    readonly methodNotSupported: -32004;
+    readonly limitExceeded: -32005;
+    readonly parse: -32700;
+    readonly invalidRequest: -32600;
+    readonly methodNotFound: -32601;
+    readonly invalidParams: -32602;
+    readonly internal: -32603;
+  };
+  readonly provider: {
+    readonly userRejectedRequest: 4001;
+    readonly unauthorized: 4100;
+    readonly unsupportedMethod: 4200;
+    readonly disconnected: 4900;
+    readonly chainDisconnected: 4901;
+  };
+}
+
+export const errorCodes: ErrorCodes = {
   rpc: {
     invalidInput: -32000,
     resourceNotFound: -32001,
