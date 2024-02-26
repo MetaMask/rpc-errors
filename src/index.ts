415c34061d3df5c2ba355734c77cc52ce86f3104
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-02-26 11:04:14 +0100

    Export `OptionalDataWithOptionalCause` type (#135)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { JsonRpcError, EthereumProviderError } from './classes';
 export { serializeCause, serializeError, getMessageFromCode } from './utils';
-export type { DataWithOptionalCause } from './utils';
+export type {
+  DataWithOptionalCause,
+  OptionalDataWithOptionalCause,
+} from './utils';
 export { rpcErrors, providerErrors } from './errors';
 export { errorCodes } from './error-constants';
